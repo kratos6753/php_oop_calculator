647cf664a47ba89d2eaa086ee248414c6e23f3b2
chiru <chiranjeevi>
2016-09-10 17:45:02 +0530

    added method to calculate mode

--- Calc.class.php
+++ Calc.class.php
@@ -31,6 +31,31 @@ class Calc implements ICalc {
 		return $sum/$num_args;
 	}
 
+	public function mode() {
+		$args = func_get_args();
+		$calc_util = $this->calc_utility($args);
+		if(is_array($calc_util)) {
+			list($num_args, $args) = $calc_util;
+			$this->radix_sort($args);
+			$mode = $args[0];
+			$max_repetition = 1;
+			$next_repetition = 0;
+			for ($i=1; $i < $num_args; $i++) {
+				if($args[$i-1] == $args[$i]) {
+					$next_repetition++;
+				} else {
+					if($max_repetition <= $next_repetition) {
+						$max_repetition = $next_repetition;
+						$mode = $args[$i-1];
+					}
+					$next_repetition = 1;
+				}
+			}
+			return $mode;
+		} else
+			return $calc_util;
+	}
+
 	private function check_numeric_array(array $some_array) {
 		$all_numeric = true;
 		$failed_index = -1;
@@ -64,5 +89,36 @@ class Calc implements ICalc {
 			$i++;
 		}
 		return $args;
+	}
+
+	private function counting_sort(array &$arr) {
+		$min = min($arr);
+		$max = max($arr);
+		$count_num = $max - $min + 1;
+		$count = array_fill(0, $count_num, 0);
+		$output = array_fill(0, count($arr), 0);
+		for ($i=0; $i < count($arr); $i++) {
+			$count[$arr[$i] - $min]++;
+		}
+		for ($i=1; $i < $count_num; $i++) {
+			$count[$i] += $count[$i-1];
+		}
+		for ($i=0; $i < count($arr); $i++) {
+			$output[$count[$arr[$i]-$min]-1] = $arr[$i];
+			$count[$arr[$i]-$min]--;
+		}
+		for ($i=0; $i < count($arr); $i++) {
+			$arr[$i] = $output[$i];
+		}
+		return $arr;
+	}
+
+	private function radix_sort(array &$arr) {
+		$max = max($arr);
+		while($max % 10) {
+			$this->counting_sort($arr);
+			$max /= 10;
+		}
+		return $arr;
 	}
 }
--- index.php
+++ index.php
@@ -8,4 +8,5 @@ spl_autoload_register(function ($class_name) {
 $calc = new Calc();
 echo 'Addition -> '.$calc->addition(1, 2, 3, 4, 5).'<br>';
 echo 'Multiplication -> '.$calc->multiply(1, 2, 3, 4, 5).'<br>';
-echo 'Mean -> '.$calc->mean(1, 2, 3, 4, 5);
+echo 'Mean -> '.$calc->mean(1, 2, 3, 4, 5).'<br>';
+echo 'Mode -> '.$calc->mode(1, 2, 3, 3, 4, 4, 4, 5);
